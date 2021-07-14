b85dbe462227b01dfaa8140a4774ae281b9e8262
virtualpatterns.com <code@virtualpatterns.com>
2021-07-13 20:52:40 -0400

    upgrades

--- source/index.js
+++ source/index.js
@@ -1,3 +1,5 @@
+const OK = true
+
 export * from './library/log.js'
 export * from './library/log/fast-log.js'
 export * from './library/log/formatted-log.js'
@@ -5,3 +7,5 @@ export * from './library/log/short-formatted-log.js'
 
 export * from './library/error/log-attached-error.js'
 export * from './library/error/log-detached-error.js'
+
+export { OK }
